6f7edb8b64730c6aed5c5cc5977485f76765ac8e
Samyy5100 <23_samiksha.shinde@ges-coengg.org>
2026-05-11 08:01:49 +0530

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -38,3 +38,30 @@ start = input("Enter start node: ")
 
 print("Shortest Distances:")
 print(dijkstra(graph, start))
+
+-----------------Output---------------------
+Enter number of nodes: 4
+
+Enter node name: A
+Enter number of edges for A: 2
+Neighbor: B
+Weight: 2
+Neighbor: C
+Weight: 5
+
+Enter node name: B
+Enter number of edges for B: 2
+Neighbor: C
+Weight: 1
+Neighbor: D
+Weight: 4
+
+Enter node name: C
+Enter number of edges for C: 1
+Neighbor: D
+Weight: 3
+
+Enter node name: D
+Enter number of edges for D: 0
+
+Enter start node: A
